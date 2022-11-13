fcee6dede0b3b97f0173bff61984d05ef216ecf4
Galina <gal-dej@yandex.ru>
2022-11-13 19:51:27 +0300

    A bit edition

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -5,10 +5,12 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+int num1 = 4;
+int num2 = 10;
 int SummNumners (int num1, int num2)
 {
     if (num1==num2)
        return num1;
     else {return num1 + SummNumners(num1+1,num2);}
 }
-Console.WriteLine(SummNumners(4,10));
+Console.WriteLine($"M = {num1}; N = {num2} -> {SummNumners(4,10)}");
