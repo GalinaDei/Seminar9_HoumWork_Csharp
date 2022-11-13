34e0a7dd1d07cc74d4bc8f56809f7c518ff82c06
Galina <gal-dej@yandex.ru>
2022-11-13 19:17:27 +0300

    New Task 66

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -5,3 +5,10 @@
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+int SummNumners (int num1, int num2)
+{
+    if (num1==num2)
+       return num1;
+    else {return SummNumners((num2-1) + num2);}
+}
+Console.WriteLine(SummNumners(2,8));
